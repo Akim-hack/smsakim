62c63cd6ea8bafd9815936df9cc27d8ed7e9548e
Akim-hack <akimfiremax2004@gmail.com>
2026-09-18 10:55:30 +0100

    Create SimulateurActivity.java

---
+++ app/src/main/java/com/akim_sms/SimulateurActivity.java
@@ -0,0 +1,33 @@
+package com.akim_sms;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimulateurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setBuiltInZoomControls(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setBackgroundColor(0xFF0A0E1A);
+
+        webView.loadUrl("file:///android_asset/simulateur.html");
+
+        setContentView(webView);
+    }
+}
